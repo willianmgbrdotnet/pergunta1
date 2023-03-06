818cec06aa941bfb7ec6b78bfa2169deed1f1a13
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-06 12:44:28 -0300

    Inicio do Projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Pegunta1;
+
+class Program
+{
+    /*Observe o trecho de código abaixo:
+    int INDICE = 13, SOMA = 0, K = 0;
+
+    enquanto K < INDICE faça
+    {
+        K = K + 1;
+
+        SOMA = SOMA + K;
+    }
+
+    imprimir(SOMA);
+
+    Ao final do processamento, qual será o valor da variável SOMA?*/
+
+    static void Main(string[] args)
+    {
+        
+    }
+
+}

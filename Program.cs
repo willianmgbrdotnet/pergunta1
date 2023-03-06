d0f08a3b9a903aad836cd240dee7e4c070df7fb4
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-06 13:09:00 -0300

    Projeto ok

--- Program.cs
+++ Program.cs
@@ -20,7 +20,28 @@ class Program
 
     static void Main(string[] args)
     {
-        
+        int indice = 13;
+        int soma = 0;
+        int k = 0;
+
+        for(k = 0; k < indice; k++)
+        {
+            soma = soma + k;
+            Console.WriteLine("O valor da variavel soma é : {0}", soma);
+        }
     }
+    /*O valor da variavel soma é : 0
+    O valor da variavel soma é : 1
+    O valor da variavel soma é : 3
+    O valor da variavel soma é : 6
+    O valor da variavel soma é : 10
+    O valor da variavel soma é : 15
+    O valor da variavel soma é : 21
+    O valor da variavel soma é : 28
+    O valor da variavel soma é : 36
+    O valor da variavel soma é : 45
+    O valor da variavel soma é : 55
+    O valor da variavel soma é : 66
+    O valor da variavel soma é : 78*/
 
 }
